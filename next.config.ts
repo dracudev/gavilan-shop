7673cf0f20c5aca59a9a72363e619761a2b1e06e
dracudev <andreujavier99@gmail.com>
2025-03-11 18:40:46 +0100

    Fixed favicon; deleted next unused rule

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["qcrmgggovhvywkzurmga.supabase.co"],
     remotePatterns: [
       {
         protocol: "https",
