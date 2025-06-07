728dba6a71100580478521f8f350f041d98001a0
dracudev <andreujavier99@gmail.com>
2025-06-07 19:20:35 +0200

    Update pathname for remote image patterns in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "qcrmgggovhvywkzurmga.supabase.co",
         port: "",
-        pathname: "/storage/v1/object/public/product-images/**",
+        pathname: "/storage/v1/object/public/**",
         search: "",
       },
     ],
